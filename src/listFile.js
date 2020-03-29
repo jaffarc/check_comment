9530ca483a4bcb9cbf69220bb6be85e73b3cf1e7
jaffar cardoso <jaffar.cardoso@gmail.com>
2020-03-29 12:19:02 -0300

    add pattern

---
+++ src/listFile.js
@@ -0,0 +1,13 @@
+var git_get_status = require('git-get-status');
+
+git_get_status(function (err, result) {
+  var status = result;
+  console.log(status)
+});
+
+// exports.fileParse  = () =>{
+
+
+
+// }
+
